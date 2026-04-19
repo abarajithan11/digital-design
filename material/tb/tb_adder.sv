5cc0b0d52a677a73e91f4e5ddca8074ca55e6af0
abarajithan11 <abarajithan07@gmail.com>
2026-04-18 22:18:39 -0700

    Change VCD path to macro

--- material/tb/tb_adder.sv
+++ material/tb/tb_adder.sv
@@ -9,8 +9,6 @@ module tb_adder;
 
   adder #(.WIDTH(WIDTH)) dut (.*);
 
-  string vcd_path;
-
   task automatic check(input logic [WIDTH-1:0] ai, input logic [WIDTH-1:0] bi);
     begin
       a = ai;
@@ -26,11 +24,8 @@ module tb_adder;
   endtask
 
   initial begin
-    if (!$value$plusargs("VCD=%s", vcd_path)) begin
-      vcd_path = "adder.vcd";
-    end
-    $dumpfile(vcd_path);
-    $dumpvars(0, tb_adder);
+    $dumpfile(`VCD_PATH);
+    $dumpvars;
 
     for (int i = 0; i < 2** WIDTH; i++) begin
       for (int j = 0; j < 2** WIDTH; j++) begin
