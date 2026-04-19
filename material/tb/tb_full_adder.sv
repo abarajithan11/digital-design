003785d7ec63137bb348d409fd499dcad33a24e7
abarajithan11 <abarajithan07@gmail.com>
2026-04-19 14:34:18 -0700

    Change tbs to 1 ns

--- material/tb/tb_full_adder.sv
+++ material/tb/tb_full_adder.sv
@@ -9,15 +9,15 @@ module tb_full_adder;
   initial begin // simulation starts
     $dumpfile(`VCD_PATH); $dumpvars;
 
-    #30 a = 0; b = 0; ci = 0;    
-    #10 a = 0; b = 0; ci = 1;
+    #3 a = 0; b = 0; ci = 0;    
+    #1 a = 0; b = 0; ci = 1;
     
-    #20 a = 1; b = 1; ci = 0;
+    #2 a = 1; b = 1; ci = 0;
     #1  assert ({co,sum} == a+b+ci) 
           $display ("OK"); 
           else $error("Not OK");
     
-    #10 a = 1; b = 1; ci = 1;
+    #1 a = 1; b = 1; ci = 1;
     #1  assert (dut.wire_1 == 0) // 1 (xor) 1 = 0 
           else $error("False. wire_1:%d", dut.wire_1);
     
--- material/tb/tb_n_adder.sv
+++ material/tb/tb_n_adder.sv
@@ -21,16 +21,16 @@ module tb_n_adder;
     A = 8'd5; B = 8'd10; ci = 0;
     #1 assert (S == 8'd15) else $error("Fail");
 
-    #10 A = 8'd30;  B = -8'd10; ci = 0;
-    #10 A = 8'd5;   B = 8'd10;  ci = 1;
-    #10 A = 8'd127; B = 8'd1;   ci = 0;
+    #1 A = 8'd30;  B = -8'd10; ci = 0;
+    #1 A = 8'd5;   B = 8'd10;  ci = 1;
+    #1 A = 8'd127; B = 8'd1;   ci = 0;
 
     repeat(10) begin
-      #9
+      #1
       status = std::randomize(ci);
       status = std::randomize(A) with { A inside {[-128:127]}; };
       status = std::randomize(B) with { B inside {[-128:127]}; };
-      #1
+      #1ps
       assert ({co,S} == A+B+ci)
         else $error("%d+%d+%d != {%d,%d}", A,B,ci,co,S);
     end
